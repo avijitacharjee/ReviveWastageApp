c272026a4e5d36826374e133463ab857d907d133
Avijit Acharjee <avijitach@gmail.com>
2020-08-08 13:02:35 +0600

    login

--- app/src/main/java/com/avijit/revivewastage/MainActivity.java
+++ app/src/main/java/com/avijit/revivewastage/MainActivity.java
@@ -6,15 +6,69 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    Button loginButton;
+    Button loginButton,signUpButton;
+    EditText emailEditText, passwordEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailEditText = findViewById(R.id.email_edit_text);
+        passwordEditText = findViewById(R.id.pasword_edit_text);
         loginButton = findViewById(R.id.button);
+        signUpButton = findViewById(R.id.signup_intent);
         loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+                String url = "https://finalproject.xyz/revive_wastage/api.php";
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if(!response.equals("success")){
+                            Toast.makeText(MainActivity.this, "Incorrect email/password", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        }){
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        return super.getHeaders();
+                    }
+
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String,String> params = new HashMap<>();
+                        params.put("login","1");
+                        params.put("email",emailEditText.getText().toString());
+                        params.put("password",passwordEditText.getText().toString());
+                        return params;
+                    }
+                };
+                requestQueue.add(stringRequest);
+            }
+        });
+        signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this,SignUp.class));
